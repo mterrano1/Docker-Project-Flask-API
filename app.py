39cc2f7ecc877674b4ecfba6a873eec2f5700795
Christopher Terrano <mterrano1@yahoo.com>
2023-02-26 19:32:28 -0600

    create review post route and query

--- app.py
+++ app.py
@@ -24,8 +24,25 @@ def reviews(item_id=None):
         reviews = Review.query.filter_by(item_id=item_id).all()
         reviews_list = [{
             "id": review.id,
-            "item_id": review.case_id,
+            "item_id": review.item_id,
             "review": review.review,
             "username": review.username,
         } for review in reviews]
         return jsonify(reviews_list)
+    
+    if request.method == "POST":
+        review_data = request.get_json()
+        review = review_data.get("review")
+        if not review or review.strip() == "":
+            return jsonify({"errors": ["Input is required"]}), 400
+        new_review = Review(
+            item_id=review_data["item_id"],
+            review=review,
+            username=review_data["username"]
+        )
+        db.session.add(new_review)
+        db.session.review()
+        return jsonify({"message": "Review added successfully"}), 201
+
+if __name__ == '__main__':
+    app.run(debug=True)
